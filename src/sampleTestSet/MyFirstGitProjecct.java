9c89993b1fb16e977e06f9a941ca7ab882e1f652
Naren65 <narendra.naidu@uatsoftsolutions.com>
2022-10-03 19:29:31 +0530

    This my Buildv1.0 commit

---
+++ src/sampleTestSet/MyFirstGitProjecct.java
@@ -0,0 +1,16 @@
+package sampleTestSet;
+
+public class MyFirstGitProjecct {
+
+	public static void main(String[] args) {
+		
+     int x=5;
+     
+     if(x<10) {
+    	 
+    	 System.out.println("X value less than 10");
+     }
+
+	}
+
+}
